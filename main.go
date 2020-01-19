544bf820d58b38a62cece7bec6eb0ca471b99362
Vorotynsky Maxim <vorotynsky.maxim@yandex.ru>
2020-01-19 22:11:15 +0300

    fix: add interrupt signals handling
    The server processes os signals to shut down correctly.

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync"
 
 	"github.com/zpatrick/go-config"
 )
@@ -15,6 +19,16 @@ func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(resp, m)
 }
 
+func signalInterruptHandle(handler func()) {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+
+	go func() {
+		<-signals
+		handler()
+	}()
+}
+
 func main() {
 	iniFile := config.NewINIFile("config.ini")
 	c := config.NewConfig([]config.Provider{iniFile})
@@ -23,6 +37,22 @@ func main() {
 	host := fmt.Sprintf("localhost:%d", port)
 
 	handler := msg("hello from go")
+
+	httpSrv := &http.Server{Addr: host, Handler: handler}
+
+	httpWG := &sync.WaitGroup{}
+	httpWG.Add(1)
+
+	go func(server *http.Server, wg *sync.WaitGroup) {
+		defer wg.Done()
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe: %v", err)
+		}
+	}(httpSrv, httpWG)
+
 	fmt.Printf("Server is listening on http://%s\n", host)
-	http.ListenAndServe(host, handler)
+	defer fmt.Println("Server has stopped listening.")
+
+	signalInterruptHandle(func() { httpSrv.Close() })
+	httpWG.Wait()
 }
